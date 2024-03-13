64f9cbdcd0bcd751c200987938b5309ab033d1b5
MasterCraftArc <mikeslaughter@defenseunicorns.com>
2024-03-13 10:13:17 -0600

    blob
    Signed-off-by: MasterCraftArc <mikeslaughter@defenseunicorns.com>

---
+++ src/lib/components/Blob.svelte
@@ -0,0 +1,25 @@
+<script>
+	import { onMount } from 'svelte';
+	import blob from '../images/svg/hero-blob.svg';
+	let blobElement;
+	onMount(() => {
+		animateBlob(blobElement);
+	});
+	function animateBlob(element) {
+		let scale = 1;
+		let direction = 1;
+		function step() {
+			scale += direction * 0.0001;
+			if (scale > 1 || scale < 0.9) {
+				direction *= -1;
+			}
+			element.style.transform = `scale(${scale})`;
+			requestAnimationFrame(step);
+		}
+		requestAnimationFrame(step);
+	}
+</script>
+
+<div class="container">
+	<img bind:this={blobElement} src={blob} alt="blob" loading="lazy" />
+</div>
